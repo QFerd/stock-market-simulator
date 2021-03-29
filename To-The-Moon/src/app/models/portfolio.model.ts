b109b67332a1b208dc9366d7be57ba96c6c431af
mjgreene2010 <mjgreene2010@gmail.com>
2021-03-29 18:24:09 -0500

    not pushing

--- To-The-Moon/src/app/models/portfolio.model.ts
+++ To-The-Moon/src/app/models/portfolio.model.ts
@@ -1,7 +1,10 @@
+import { Position } from './position.model'
+
 export interface Portfolio{
     portfolio_id:number;
     cash_value:number;
     stock_value:number;
     total_value:number;
+    positions: Position[];
     
 }
