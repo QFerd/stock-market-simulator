fc9a04fe038748d5fff920dbb8628dc301a67430
christian-downs <christian.downs.15@gmail.com>
2021-03-28 21:00:39 -0500

    fixed merge conflicts in code

--- To-The-Moon/src/app/mock-user.ts
+++ To-The-Moon/src/app/mock-user.ts
@@ -1,10 +1,10 @@
 import { User } from './models/user.model';
 
 export const USERS: User[] = [
-    {id:1,username:'Ron',role:'Student',portfolio_id:1,userrole_id:2},
-    {id:2,username:'Ricardo',role:'Student',portfolio_id:1,userrole_id:2},
-    {id:3,username:'Ramon',role:'Student',portfolio_id:1,userrole_id:2},
-    {id:4,username:'Barry',role:'Teacher',portfolio_id:1,userrole_id:2}
+    { id: 1, username: 'Ron', role: 'Student', portfolio_id: 1, game_game_id:2},
+    { id: 2, username: 'Ricardo', role: 'Student', portfolio_id: 1, game_game_id:2},
+    { id: 3, username: 'Ramon', role: 'Student', portfolio_id: 1, game_game_id:2},
+    { id: 4, username: 'Barry', role: 'Teacher', portfolio_id: 1, game_game_id:2}
 
 
 ]
