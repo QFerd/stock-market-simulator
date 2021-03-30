1eeac85fe4e4b0ed384604874d96ce731216c874
Sophie Lavigne <Sophie.Lavigne@yahoo.com>
2021-03-30 02:52:17 -0400

    Added stubs for Repo level unit tests. Implementation will require intelligent use of Mockito.

---
+++ Stock-Market-Sim/src/main/java/com/revature/mocktests/PositionRepoTest.java
@@ -0,0 +1,38 @@
+package com.revature.mocktests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+public class PositionRepoTest {
+	
+	@Before
+	public void init() {
+	    MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testSuiteTest() { //A test of the JUnit Testing Suite.
+		assertEquals("This is a test.", "This is a test.");
+	}
+	
+	/*
+	 * 	@Override
+	public void createOrUpdatePosition(Position position) {
+		logger.info("Attempting to create or update position with PositionRepository.");
+		sessionFactory.getCurrentSession().saveOrUpdate(position);
+	}
+	 */
+	
+	/* 
+	 * public Position(int positionId, String stockSymbol, int quantity) {
+		super();
+		this.positionId = positionId;
+		this.stockSymbol = stockSymbol;
+		this.quantity = quantity;
+	}
+	 */
+
+}
